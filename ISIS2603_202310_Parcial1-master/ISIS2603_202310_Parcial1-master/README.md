5beb29a1314be5e85c20d07e3c6711a652382c0a
sebastianrr2 <98624879+sebastianrr2@users.noreply.github.com>
2023-03-07 18:37:12 -0500

    Add files via upload

---
+++ ISIS2603_202310_Parcial1-master/ISIS2603_202310_Parcial1-master/README.md
@@ -0,0 +1,3 @@
+# Parcial práctico 1
+
+Siga las instrucciones proporcionadas por el profesor de su sección.
---
+++ ISIS2603_202310_Parcial1-master/ISIS2603_202310_Parcial1-master/src/main/java/co/edu/uniandes/dse/parcial1/repositories/CascaraRepository.java
@@ -0,0 +1,8 @@
+package co.edu.uniandes.dse.parcial1.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CascaraRepository {
+
+}
